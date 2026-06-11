f38c7ba0c8c6556f1bc294312c4167c4f9f77263
yukid <yukidate05@gmail.com>
2026-06-11 14:31:56 +0900

    fix: bump versionCode to 3

--- app.config.ts
+++ app.config.ts
@@ -24,7 +24,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   android: {
     package: 'com.yukid.polaris',
-    versionCode: 2,
+    versionCode: 3,
     googleServicesFile: './google-services.json',
     adaptiveIcon: {
       foregroundImage: './assets/adaptive-icon.png',
