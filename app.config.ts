1f90af0621e2adec42047b6d6df4f46e4f2ae78b
yukid <yukidate05@gmail.com>
2026-06-11 13:41:46 +0900

    fix: set versionCode 2 in app.config.ts android section

--- app.config.ts
+++ app.config.ts
@@ -24,6 +24,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   android: {
     package: 'com.yukid.polaris',
+    versionCode: 2,
     googleServicesFile: './google-services.json',
     adaptiveIcon: {
       foregroundImage: './assets/adaptive-icon.png',
